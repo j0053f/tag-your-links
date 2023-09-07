4cf36626427b0c3fa28fa5d33a130ed4d215fefa
j0053f <jooo3f@gmail.com>
2023-09-07 19:31:26 +0330

    bug fix: if the image URL contains query parameters, generating a file name based on it causes some problem

--- routes/storeimage-internal.js
+++ routes/storeimage-internal.js
@@ -2,16 +2,25 @@ const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
 const router = require("express").Router();
-router.get("/storeimage", async (req, res) => {
-  const imageUrl = req.query.url;
-  try {
-    const response = await axios.get(imageUrl, { responseType: "arraybuffer" });
 
-    const filename = Date.now() + path.basename(imageUrl);
-    const imagePath = path.join(__dirname, "..", "images", filename);
+function genFilenameFromURL(imageURL) {
+  const urlObj = new URL(imageURL);
+  const segments = urlObj.pathname.split("/");
+  const filename = segments[segments.length - 1];
+  return filename;
+}
+
+router.get("/storeimage", async (req, res) => {
+  const imageURL = req.query.url;
+  const filename = genFilenameFromURL(imageURL);
+  try {
+    const response = await axios.get(imageURL, { responseType: "arraybuffer" });
+
+    const uniqueFilename = Date.now() + filename;
+    const imagePath = path.join(__dirname, "..", "images", uniqueFilename);
 
     fs.writeFileSync(imagePath, response.data);
-    res.send({ imagePath: path.join("images", filename) });
+    res.send({ imagePath: path.join("images", uniqueFilename) });
   } catch (error) {}
 });
 
