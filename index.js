80c723f6837350966626721b293bfa854a73a2d3
j0053f <jooo3f@gmail.com>
2023-10-07 06:59:34 +0330

    json middleware

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(express.json());
 app.use(morgan("tiny"));
 
 app.use("/api", linkpreviewRouter);
